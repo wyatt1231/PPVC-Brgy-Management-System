0e4451379be20f342ef3347bf8e65f7429431122
rward2331 <edwardjosephfernandez@gmail.com>
2021-07-06 16:22:25 +0800

    changes

--- src/Configurations/DatabaseConfig.ts
+++ src/Configurations/DatabaseConfig.ts
@@ -9,11 +9,11 @@ export let connection_string: mysql.PoolOptions | null = null;
 
 if (process.env.NODE_ENV === "production") {
   connection_string = {
-    host: "127.0.0.1",
-    user: "root",
-    password: "root sa",
-    database: "bms",
-    port: 3309,
+    host: "156.67.222.35",
+    user: "u583403240_bms",
+    password: "BMS@capstone2",
+    database: "u583403240_bms",
+    port: 3306,
     connectionLimit: 10,
     waitForConnections: true,
     queueLimit: 10,
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
   connection_string = {
     host: "127.0.0.1",
     user: "root",
-    password: "root sa",
+    password: "rootsa",
     database: "bms",
     port: 3309,
     connectionLimit: 10,
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   //   waitForConnections: true,
   //   queueLimit: 10,
   // };
-}0
+}
 
 const DatabaseConfig: mysql.Pool = mysql.createPool(connection_string);
 
